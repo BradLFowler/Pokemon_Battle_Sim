8e1cab5d2e25ba84485fdbac11d9913322868e0a
BradLFowler <BradLF00@gmail.com>
2022-08-30 19:38:18 -0500

    fixing things

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import Router from './src/Router'
+import Router from './Router'
 
-import App from './src/App';
+import App from './App';
 
 import './App.css';
 
